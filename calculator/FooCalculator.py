37a27aa847821b0143b1938bb1640d8d5d0b9712
glc-78 <caramelleamare@gmail.com>
2018-10-04 01:14:40 +0200

    added unit test: sum and divide OK

--- calculator/FooCalculator.py
+++ calculator/FooCalculator.py
@@ -6,7 +6,7 @@ class FooCalculator:
         pass
 
     def sum(self, m, n):
-        return c.sum(m,n)
-
+        return c.sum(m, n)
+        
     def divide(self, m, n):
         return c.divide(m, n)
